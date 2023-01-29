27c6d23c71f29db98c7447e10adf0fe53308a12f
rickaed <wcsrickaed22@gmail.com>
2023-01-29 13:53:05 +0100

    try ngfor display

--- src/app/developer/developer.component.ts
+++ src/app/developer/developer.component.ts
@@ -22,9 +22,9 @@ export class DeveloperComponent implements OnInit {
     age: 35,
     gender: "male",
     bio: "dev depuis 10 ans, orienté fullstack",
-    skill: [this.js]
+    skill: [this.js,this.css]
   };
-
+devskills:Skill[] = this.dev.skill
 
   ngOnInit() {
 
@@ -33,11 +33,11 @@ export class DeveloperComponent implements OnInit {
       logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1024px-Unofficial_JavaScript_logo_2.svg.png",
       site: "https://developer.mozilla.org/fr/docs/Web/JavaScript"
     };
-    // this.css = {
-    //   name: "CSS",
-    //   logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/langfr-800px-CSS3_logo_and_wordmark.svg.png",
-    //   site: "https://developer.mozilla.org/fr/docs/Web/CSS"
-    // };
+    this.css = {
+      name: "CSS",
+      logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/langfr-800px-CSS3_logo_and_wordmark.svg.png",
+      site: "https://developer.mozilla.org/fr/docs/Web/CSS"
+    };
     // this.angular = {
     //   name: "Angular",
     //   logo: "https://as2.ftcdn.net/v2/jpg/02/95/03/85/1000_F_295038583_mn0uxJ6A0YO57HA4xXQqHFUjiW1BcqBE.jpg",
